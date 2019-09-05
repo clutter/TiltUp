90f543f7b8acd1e98bba953d606a40112f1da7f1
Erik Strottmann <erik.strottmann@clutter.com>
2019-09-05 12:24:26 -0700

    Add test helpers for expectations and main thread

---
+++ TiltUpTest/Assertions/XCTestCase+wait.swift
@@ -0,0 +1,15 @@
+//
+//  XCTestCase+wait.swift
+//  ForkliftTests
+//
+//  Created by Jeremy Grenier on 4/3/19.
+//  Copyright © 2019 Clutter. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func wait(for expectations: [XCTestExpectation]) {
+        wait(for: expectations, timeout: 1.0)
+    }
+}
